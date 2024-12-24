e930603e7c304e32618dbae305ae50f99cb11d16
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2024-12-24 22:16:59 +0600

    cv

--- src/Components/Compents/MobileNavigation/MobileNavigation.jsx
+++ src/Components/Compents/MobileNavigation/MobileNavigation.jsx
@@ -16,12 +16,37 @@ import { AnimatePresence, delay, motion, stagger } from "framer-motion";
 const categoryList = [
   {
     id: 1,
-    name: "Home",
+    name: "SALE",
     to: "#",
   },
   {
     id: 2,
-    name: "About",
+    name: "MEN",
+    to: "#",
+  },
+  {
+    id: 3,
+    name: "WOMEN",
+    to: "#",
+  },
+  {
+    id: 4,
+    name: "KIDS",
+    to: "#",
+  },
+  {
+    id: 5,
+    name: "NEWBORN",
+    to: "#",
+  },
+  {
+    id: 6,
+    name: "ACCESSORIES",
+    to: "#",
+  },
+  {
+    id: 7,
+    name: "FOOTWEAR",
     to: "#",
   },
 ];
@@ -75,9 +100,9 @@ export default function MobileNavigation({ navOpen, setNavOpen }) {
           <div className="w-[200vw]">
             <motion.div
               {...animation(navCatList)}
-              className="flex h-screen w-screen flex-col items-center justify-center bg-red-700"
+              className="flex h-screen w-screen flex-col items-center justify-center bg-slate-800"
             >
-              <div className="mx-auto mt-4 flex w-[60%] flex-col justify-start gap-3">
+              <div className="mt-4 flex w-[60%] flex-col justify-start gap-3">
                 {categoryList.map((category) => (
                   <div
                     onClick={() => handleOpen(category.id)}
@@ -91,6 +116,14 @@ export default function MobileNavigation({ navOpen, setNavOpen }) {
                   </div>
                 ))}
               </div>
+
+              {/* Close Button */}
+              <div
+                onClick={() => setNavOpen(false)}
+                className="absolute bottom-0 mb-[56px] flex w-full justify-center bg-red-800 py-4 text-lg text-red-100"
+              >
+                Close
+              </div>
             </motion.div>
             {open && (
               <motion.div
@@ -99,6 +132,21 @@ export default function MobileNavigation({ navOpen, setNavOpen }) {
               >
                 ddddd
                 <button onClick={handleClick}>dd</button>
+                {/*Back & Close Button */}
+                <div className="absolute bottom-0 mb-[56px] flex w-screen flex-row bg-slate-900 text-slate-50">
+                  <div
+                    onClick={() => setOpen(false)}
+                    className="flex w-full justify-center border-r-2 border-slate-100 py-4 text-lg text-red-100"
+                  >
+                    Back
+                  </div>
+                  <div
+                    onClick={() => setNavOpen(false)}
+                    className="flex w-full justify-center py-4 text-lg text-red-100"
+                  >
+                    Close
+                  </div>
+                </div>
               </motion.div>
             )}
           </div>
@@ -107,48 +155,3 @@ export default function MobileNavigation({ navOpen, setNavOpen }) {
     </AnimatePresence>
   );
 }
-
-// <motion.div
-//         {...anima(navMobile)}
-//         className="relative flex items-center justify-center"
-//       >
-//         <div className="fixed left-0 top-0 z-[100] flex h-screen flex-col justify-start bg-red-800 lg:hidden">
-//           <div className="mt-4 w-screen p-2">
-//             {/* Header Logo section */}
-//             <div className="flex flex-col items-center justify-center">
-//               <div className="flex items-center justify-center">
-//                 <img src="./Picky.svg" alt="logo" className="h-8 md:h-12" />
-//               </div>
-//               <span className="mt-2 block h-[2px] w-[60vw] bg-slate-800" />
-//             </div>
-
-//             {/* Navigation Links */}
-//             <div className="flex flex-row">
-//               <motion.div className="w-screen" {...animation(navCatList)}>
-//                 <div className="mx-auto mt-4 flex w-[60%] flex-col justify-start gap-3">
-//                   {categoryList.map((category) => (
-//                     <div
-//                       onClick={() => handleOpen(category.id)}
-//                       className="flex items-center justify-center bg-slate-50"
-//                       key={category.id}
-//                       style={{
-//                         clipPath: "polygon(2% 0, 100% 0, 98% 100%, 0 100%)",
-//                       }}
-//                     >
-//                       <p className="px-2 py-1">{category.name}</p>
-//                     </div>
-//                   ))}
-//                 </div>
-//               </motion.div>
-//               <motion.div
-//                 className="h-screen w-screen bg-slate-100"
-//                 {...animation(navCategory)}
-//               >
-//                 {open && (
-//                   <div className="mt-4">{renderMobileNavCategory()}</div>
-//                 )}
-//               </motion.div>
-//             </div>
-//           </div>
-//         </div>
-//       </motion.div>
