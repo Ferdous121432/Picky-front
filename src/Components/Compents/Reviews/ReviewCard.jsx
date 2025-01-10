b5b0ff6367941a9ccb1fab25eae3ab0f738131fa
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2025-01-11 02:41:10 +0600

    n

---
+++ src/Components/Compents/Reviews/ReviewCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function ReviewCard() {
+  return (
+    <div>
+      <div>
+        <h1>Esteban Reardon</h1>
+        <FontAwesomeIcon icon={faStar} />
+      </div>
+    </div>
+  );
+}
--- src/Components/Pages/ProductPage/SizeGuide.jsx
+++ src/Components/Pages/ProductPage/SizeGuide.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function SizeGuide() {
-  return <div>SizeGuide</div>;
+  return (
+    <div>
+      <h2>Size Guide</h2>
+      <h3>Men</h3>
+      <ul>
+        <li>Small: Chest 34-36 inches, Waist 28-30 inches</li>
+        <li>Medium: Chest 38-40 inches, Waist 32-34 inches</li>
+        <li>Large: Chest 42-44 inches, Waist 36-38 inches</li>
+        <li>X-Large: Chest 46-48 inches, Waist 40-42 inches</li>
+      </ul>
+
+      <h3>Women</h3>
+      <ul>
+        <li>Small: Bust 32-34 inches, Waist 24-26 inches, Hips 34-36 inches</li>
+        <li>
+          Medium: Bust 36-38 inches, Waist 28-30 inches, Hips 38-40 inches
+        </li>
+        <li>Large: Bust 40-42 inches, Waist 32-34 inches, Hips 42-44 inches</li>
+        <li>
+          X-Large: Bust 44-46 inches, Waist 36-38 inches, Hips 46-48 inches
+        </li>
+      </ul>
+
+      <h3>Children</h3>
+      <ul>
+        <li>2-3 Years: Height 36-38 inches, Weight 30-33 lbs</li>
+        <li>4-5 Years: Height 40-43 inches, Weight 34-38 lbs</li>
+        <li>6-7 Years: Height 45-48 inches, Weight 39-44 lbs</li>
+        <li>8-9 Years: Height 50-53 inches, Weight 45-50 lbs</li>
+      </ul>
+    </div>
+  );
 }
